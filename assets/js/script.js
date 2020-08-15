fc6818e13d11967f2401019a5925bfd765a4a357
dwikyls <kreuzvernicterlanze@gmail.com>
2020-08-15 18:25:25 +0700

    ngerapihin dan ngehapus file duplikat

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,20 +6,24 @@ const getData = () => {
         $.each(okoc, (i, data) => {
             $("#video-list").append(`
             <div class="row justify-content-center pt-5 pb-5 font-cal" id="kelas${data.kelas}">
-                <figure class="text-center">
-                    <iframe width="560" height="315" src="${data.video}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    <figcaption><h3>Materi SD Kelas ${data.kelas}</h3></figcaption>
-                </figure>
+                <div class="col-10 col-md-8 col-lg-6 shadow card p-3">
+                    <figure class="text-center">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" src="${data.video}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                        <figcaption><h3>Materi SD Kelas ${data.kelas}</h3></figcaption>
+                    </figure>
+                <div>
             </div>
             `);
         });
 
-        $.each(okoc2, (i, data) =>{
-            $('#mentor-list').append(`
-            <div class="col-lg-2 tengah">
+        $.each(okoc2, (i, data) => {
+            $("#mentor-list").append(`
+            <div class="col-lg-2 text-center">
                 <figure>
-                    <img src="${data.img}" class="rounded-circle img-fluid">
-                    <figcaption><h3>${data.nama}</h3></figcaption>
+                    <img src="${data.img}" class="shadow rounded-circle img-fluid">
+                    <figcaption class="pt-2">${data.nama}</figcaption>
                 </figure>
             </div>
             `);
