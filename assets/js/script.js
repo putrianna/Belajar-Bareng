d857f4c004186f60f405bd1f17b109927aceceb5
dwikyls <kreuzvernicterlanze@gmail.com>
2020-08-15 16:34:51 +0700

    memperbaiki page 2,3,4 dalam mode hp

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,23 +5,23 @@ const getData = () =>{
         
         $.each(okoc, (i, data)=>{
             $('#video-list').append(`
-            <div class="row justify-content-center pt-5 pb-5 font-cal" id="kelas${data.kelas}">
-                <figure class="text-center">
-                    <iframe width="560" height="315" src="${data.video}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    <figcaption><h3>Materi SD Kelas ${data.kelas}</h3></figcaption>
-                </figure>
-            </div>
+                <div class="row justify-content-center pt-5 pb-5 font-cal" id="kelas${data.kelas}">
+                    <figure class="text-center">
+                        <iframe width="560" height="315" src="${data.video}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <figcaption><h3>Materi SD Kelas ${data.kelas}</h3></figcaption>
+                    </figure>
+                </div>
             `)
         })
 
         $.each(okoc2, (i, data) =>{
             $('#mentor-list').append(`
-            <div class="col-lg-2 tengah">
-                <figure>
-                    <img src="${data.img}" class="rounded-circle img-fluid">
-                    <figcaption><h3>${data.nama}</h3></figcaption>
-                </figure>
-            </div>
+                <div class="col-lg-2 tengah">
+                    <figure>
+                        <img src="${data.img}" class="rounded-circle img-fluid">
+                        <figcaption><h3>${data.nama}</h3></figcaption>
+                    </figure>
+                </div>
             `)
         })
     })    
